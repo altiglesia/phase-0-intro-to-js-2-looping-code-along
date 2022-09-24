984e0429aa7af897da3d0c7d9cabf08e3607cfa9
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-24 18:13:22 -0400

    commit tree a3434f588e90804ff1babef63551244a4291cfcb

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards (namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        console.log(thankYouCards.push (`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`));
+    }
+    return thankYouCards;
+}
+writeCards(namesArray,'surprise');
+
+function countDown (startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber);
+}
